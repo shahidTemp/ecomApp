ad5dc806c2302134d1dd96f281dd4e7449b7b0af
shaid <mdshahid7384@gmail.com>
2026-08-22 07:03:51 +0600

    start

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,32 +1,31 @@
-import { Text, View } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient";
-import SearchTrigger from '@/components/common/searchTrigger'
-import ModernCarousel from '@/components/home/carousel'
-import { useTheme } from '@/lib/theme'
+import SearchTrigger from "@/components/common/searchTrigger";
+import ModernCarousel from "@/components/home/carousel";
+import { useTheme } from "@/lib/theme";
 
 const Index = () => {
-    const { theme } = useTheme()
+  const { theme } = useTheme();
 
-    return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <LinearGradient
-                colors={[theme.bgcolor, `${theme.bgcolor}10`, "#ffffff"]}
-                locations={[0, 0.4, 1]}
-                start={{ x: 0, y: 0 }}
-                end={{ x: 0, y: 1 }}
-                className="flex-1"
-            >
-                <StatusBar backgroundColor={theme.bgcolor} style="light" />
-                <View className="px-3 pt-3 gap-4">
-                    <SearchTrigger />
-                    <ModernCarousel />
-                    <Text>index</Text>
-                </View>
-            </LinearGradient>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <LinearGradient
+        colors={[theme.bgcolor, `${theme.bgcolor}10`, "#ffffff"]}
+        locations={[0, 0.4, 1]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+        className="flex-1"
+      >
+        <StatusBar backgroundColor={theme.bgcolor} style="light" />
+        <View className="px-3 pt-3 gap-4">
+          <SearchTrigger />
+          <ModernCarousel />
+        </View>
+      </LinearGradient>
+    </SafeAreaView>
+  );
+};
 
-export default Index
+export default Index;
